cd9bd103c4e4b9348534eb93628bc6ba24a26d1b
Nagesh Gorre <nageshgorre007@gmail.com>
2026-06-02 16:32:18 +0530

    api key fetch from env

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,9 @@
+import os
 
 BASE_URL = "https://opensource-demo.orangehrmlive.com/"
 USERNAME = "Admin"
 PASSWORD = "admin123"
-GEMINI_API_KEY = ""
+GEMINI_API_KEY = os.getenv(
+    "GEMINI_API_KEY"
+)
 MODEL_NAME = "gemini-2.5-flash"
